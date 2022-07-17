1abb0cbe5744b3f4558760569eebfe963d281fd6
Michał Konowalski <konowalski.michal@gmail.com>
2022-07-17 14:34:42 +0200

    WA-001 delete notes

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -6,17 +6,8 @@ public class Application {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         userInterface.run();
-
     }
 }
 
 
 
-/*
-* notatki
-* ctrl + t - update project
-* alt + 9 - historia commitow
-* ctrl shift tylda - branche, utworzenie nowego brancza (np. develop) w tym oknie
-* ctrl shift k - push
-* ctrl k - commit
-* */
