29fbaf45d9a26a39c8000cb032d8f9282dd94da4
Michał Konowalski <konowalski.michal@gmail.com>
2022-07-17 12:47:04 +0200

    Initial commit

---
+++ src/main/java/com/sda/Application.java
@@ -0,0 +1,17 @@
+package com.sda;
+
+
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+
+
+/*
+* notatki
+*
+*
+* */
