ec86638453ccee9dd602fb08debabd923e212a59
Michał Konowalski <konowalski.michal@gmail.com>
2022-07-17 13:51:45 +0200

    WA-001 create UserInterface

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -4,8 +4,9 @@ package com.sda;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Michal tu byl");
+        UserInterface userInterface = new UserInterface();
+        userInterface.run();
+
     }
 }
 
@@ -13,6 +14,9 @@ public class Application {
 
 /*
 * notatki
-*
-*
+* ctrl + t - update project
+* alt + 9 - historia commitow
+* ctrl shift tylda - branche, utworzenie nowego brancza (np. develop) w tym oknie
+* ctrl shift k - push
+* ctrl k - commit
 * */
